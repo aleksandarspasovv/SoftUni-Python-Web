15bd1aa6187e678fed5c8c718db0fdcfb2e3c09a
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-09-21 21:56:17 +0300

    updates

--- Django-Basics/djangointroduction/djangointroduction/todo_app/admin.py
+++ Django-Basics/djangointroduction/djangointroduction/todo_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from djangointroduction.todo_app.models import Task
+
+
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
